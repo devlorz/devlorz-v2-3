7358b30d163fbc57ccdb13c5f1b6a17790a2553d
Nutti Saelor <leelorz6@gmail.com>
2018-09-19 17:08:37 +0700

    - add description text - add link to social

--- src/pages/index.js
+++ src/pages/index.js
@@ -51,26 +51,62 @@ const Avatar = styled('img')`
 `;
 
 const Wrapper = styled('div')`
-  ${tw`flex flex-grow flex-col justify-center items-center`};
+  ${tw`flex flex-grow flex-col justify-center items-center font-sans`};
 `;
 
 const IconList = styled('div')`
   ${tw`m-4`};
 `;
 
+const TextWrap = styled('div')`
+  ${tw`mt-5 text-center`};
+`;
+
+const Text = styled('p')`
+  ${tw`m-0 md:text-base text-sm`};
+`;
+
+const textStyle = css`
+  text-decoration: none;
+  color: black;
+`;
+
 const iconStyle = css`
   margin: 5px 10px;
+  color: black;
 `;
 
 export default () => (
   <Layout>
     <Wrapper>
       <Avatar src={profilePic} alt="Nutti Saelor" />
+      <TextWrap>
+        <Text>Hello, my name is Nutti Saelor 👓.</Text>
+        <Text>
+          {' '}
+          I'm Frontend Developer 💻 and Creator of{' '}
+          <a
+            href="https://www.facebook.com/devnoteio/"
+            target="_blank"
+            className={textStyle}
+          >
+            DevNote 📓.
+          </a>
+        </Text>
+      </TextWrap>
       <IconList>
-        <FontAwesomeIcon className={iconStyle} icon={faGithub} size="2x" />
-        <FontAwesomeIcon className={iconStyle} icon={faMedium} size="2x" />
-        <FontAwesomeIcon className={iconStyle} icon={faTwitter} size="2x" />
-        <FontAwesomeIcon className={iconStyle} icon={faLinkedin} size="2x" />
+        <a href="https://github.com/devlorz">
+          <FontAwesomeIcon className={iconStyle} icon={faGithub} size="2x" />
+        </a>
+        <a href="https://medium.com/@leelorz6">
+          <FontAwesomeIcon className={iconStyle} icon={faMedium} size="2x" />
+        </a>
+        <a href="https://twitter.com/JessHolicz">
+          <FontAwesomeIcon className={iconStyle} icon={faTwitter} size="2x" />
+        </a>
+        <a href="https://www.linkedin.com/in/nuttisaelor/">
+          <FontAwesomeIcon className={iconStyle} icon={faLinkedin} size="2x" />
+        </a>
       </IconList>
     </Wrapper>
   </Layout>
