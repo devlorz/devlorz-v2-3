b78bfb996d7b9a74c8d3b99169548b0262150d2c
Nutti Saelor <leelorz6@gmail.com>
2018-10-05 00:06:59 +0700

    add pwa support

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,20 @@ module.exports = {
       options: {
         username: `@leelorz6`
       }
-    }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Nutti Saelor Portfolio Web`,
+        short_name: `DevLorz`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#4dc0b5`,
+        display: `standalone`,
+        orientation: `portrait`,
+        icon: `src/assets/icon.png` // This path is relative to the root of the site.
+      }
+    },
+    `gatsby-plugin-offline`
   ]
 };
